4d08083bacc8d35639b2501d2d715b37f195fcbe
AbhishekGaire <abhisekgaire7@gmail.com>
2024-08-14 13:06:22 +0545

    SPA Started

---
+++ worldwise/src/App.jsx
@@ -0,0 +1,28 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import Pricing from "./pages/Pricing";
+import Product from "./pages/Product";
+import PageNotFound from "./pages/PageNotFound";
+import AppLayout from "./pages/AppLayout";
+import Login from "./pages/Login";
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="product" element={<Product />} />
+        <Route path="login" element={<Login />} />
+        <Route path="app" element={<AppLayout />}>
+          <Route index element={<p>List Of Cities</p>} />
+          <Route path="cities" element={<p>Hello Cities</p>} />
+          <Route path="countries" element={<p>Hello Countries</p>} />
+          <Route path="form" element={<p>Welcome Form</p>} />
+        </Route>
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
